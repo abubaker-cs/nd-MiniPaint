10f616289e010b8c1339cec54872c25c01457fa5
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-28 02:27:14 +0500

    Initial commit

---
+++ app/src/main/java/org/abubaker/minipaint/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.abubaker.minipaint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
